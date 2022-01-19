e9099507ba6c6cfb076128b5bc5b1efb3badf3c2
Leo Chen <chenl3@rose-hulman.edu>
2022-01-19 14:01:17 -0800

    Commit message

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -1,9 +1,20 @@
 # Controller logic: fallback requests for React Router.
 # Leave this here to help deploy your app later!
-class FallbackController < ActionController::Base
+# class FallbackController < ActionController::Base
 
+#   def index
+#     # React app index page
+#     render file: 'public/index.html'
+#   end
+# end
+
+
+class FallbackController < ApplicationController
+  include ActionController::MimeResponds
+  
   def index
-    # React app index page
-    render file: 'public/index.html'
+    respond_to do |format|
+      format.html { render body: Rails.root.join('public/index.html').read }
+    end
   end
 end
