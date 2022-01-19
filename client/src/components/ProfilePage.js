934e100c56ffd1d5e16f0a13698b2490fcefe264
Leo Chen <chenl3@rose-hulman.edu>
2022-01-19 15:58:59 -0800

    Commit message

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -183,7 +183,7 @@ export default function ProfilePage({user}) {
                                 <div>
 
                                 <br/>
-                                <h5>Your current BMI: {(weight / ((height * 0.01)^2)).toFixed(0)}</h5>
+                                <h5>Your current BMI: {(weight / ((height * 0.01)^2)).toFixed(1)}</h5>
                                 
                                 <p className="text-muted">*Please note that BMI is not always an accurate indicator of health*</p>
                                 <img src="https://scontent-lga3-2.xx.fbcdn.net/v/t1.15752-9/269885323_931586591060761_5927827463391069206_n.png?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_ohc=0nFzWryjdJ8AX8GwEcj&tn=J0W_7GHedW-k0UGO&_nc_ht=scontent-lga3-2.xx&oh=03_AVJJVxCVNFULAeM8ey76nMf2KhvUAdLNTXvFkgQ4KtXtLw&oe=61F986FA" alt="BMI_Chart" width="150"/>
