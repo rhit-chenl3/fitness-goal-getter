fa8fe4d65a4f7b841c4b83e0ed7ebd55e37f36b6
Leo Chen <chenl3@rose-hulman.edu>
2022-03-17 15:43:47 -0700

    add guest credentials

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -31,7 +31,11 @@ export default function HomePage({user}) {
                         )}
                     </>
                     ) : (
-                        <div>Please sign up or log in to access app functionality!</div>   
+                    <>
+                        <div>Please sign up or log in to access app functionality!</div>
+                        <div>Feel free to login using guest credentials (username: chenl3, password: test)</div> 
+                    </>
+                          
                 )}
             </div>
         </div>
